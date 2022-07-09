7fe0d9a9c10e3a4f05c17fce647f9ce8f88c4f4b
Anna Tereshonok <ananatereshonok@yandex.ru>
2022-07-09 13:06:04 +0300

    Added default secret key to django project

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,6 +1,8 @@
 import os
 
-SECRET_KEY = os.getenv("SECRET_KEY")
+from django.core.management.utils import get_random_secret_key
+
+SECRET_KEY = os.getenv('SECRET_KEY') or get_random_secret_key()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
